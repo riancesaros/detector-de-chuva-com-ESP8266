9e8ee7a238f5036c8b774fb85fd513b174d1c0c8
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-16 23:59:47 -0300

    Update sketch_umidade_esp8266.ino

--- sketch_umidade_esp8266/sketch_umidade_esp8266.ino
+++ sketch_umidade_esp8266/sketch_umidade_esp8266.ino
@@ -1,3 +1,4 @@
+/*
 void setup() {
   // put your setup code here, to run once:
   pinMode(5, OUTPUT);
@@ -6,6 +7,7 @@ void setup() {
   pinMode(2, OUTPUT);
 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:,
@@ -29,4 +31,51 @@ void loop() {
   delay(500);
   digitalWrite(5, LOW);
   delay(500);
+}
+*/
+
+void setup() {
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int val = analogRead(A0);
+  Serial.println(val);
+
+  if (val < 500) {
+    digitalWrite(2, HIGH);
+  } else {
+    digitalWrite(2, LOW);
+  }
+
+  if (val < 200) {
+    digitalWrite(2, HIGH);
+    digitalWrite(0, HIGH);
+  } else {
+    digitalWrite(0, LOW);
+  }
+
+  if (val < 150) {
+    digitalWrite(2, HIGH);
+    digitalWrite(0, HIGH);
+    digitalWrite(4, HIGH);
+  } else {
+    digitalWrite(4, LOW);
+  }
+
+  if (val < 100) {
+    digitalWrite(2, HIGH);
+    digitalWrite(0, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+  } else {
+    digitalWrite(5, LOW);
+  }
+
+  delay(1000);
 }
